2059540dbe9f70ab22b65a88087d538a4b4d754c
Marina Esteves <113151407+Mecoaliza@users.noreply.github.com>
2023-03-08 18:30:01 -0300

    Variável reader não pegou

--- job.py
+++ job.py
@@ -4,7 +4,7 @@ import sqlite3
 
 # Criar um novo banco de dados
 
-conn = sqlite3.connect('dsabd.bd')
+conn = sqlite3.connect('dsadb.db')
 
 # Criar uma tabela para armazenar os dados 
 conn.execute('''CREATE TABLE producao (
@@ -19,3 +19,32 @@ conn.execute('''CREATE TABLE producao (
 
 conn.commit()
 conn.close()
+
+# Abre o arquivo csv 
+
+with open('producao_alimentos.csv','r') as file:
+ 
+#  Cria um leitor do CSV
+
+reader = csv.reader(file)
+
+# Pula a primeira linha do cabeçalho
+
+next(reader)
+
+# Conecta com o banco de dados 
+
+conn = sqlite3.connect('dsadb.db')
+
+# Inseri cada linha do arquivo na tabela do banco de dados
+
+for row in reader:
+    conn.execute('INSERT INTO producao (produto, quantidade, preco_medio, receita_total) VALUES (?,?,?,?)', row)
+
+conn.commit()
+conn.close()
+
+print("Job Concluído com Sucesso!")
+
+
+
